f1a764619c11cbcdfed5ef251267c0ae8bb4346b
Anakinxv <emmanuel032503@gmail.com>
2025-09-01 02:41:33 -0400

    feat: added file upload to the personalization modal, because why not 🙃

---
+++ components/CommonComponents/BubbleSelects.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "../ui/label";
+
+type BubbleSelectsProps = {
+  label?: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+};
+
+function BubbleSelects({ label, options }: BubbleSelectsProps) {
+  return (
+    <div className="w-full flex flex-col mb-4" style={{ zIndex: 1100 }}>
+      {label && (
+        <Label className="mb-1 text-left text-lg text-[var(--theme-text)]">
+          {label}
+        </Label>
+      )}
+      <Select>
+        <SelectTrigger className="w-full theme-bg-surface theme-border shadow-lg rounded-4xl p-4 min-h-[60px] text-[var(--theme-text)] focus:ring-0 focus:border-[var(--theme-primary)] focus:outline-none placeholder:text-[var(--theme-textSecondary)]">
+          <SelectValue placeholder="Select an option" />
+        </SelectTrigger>
+        <SelectContent className="theme-bg-surface theme-border  theme-text-primary shadow-lg rounded-2xl">
+          <SelectGroup>
+            {options.map((option) => (
+              <SelectItem key={option.value} value={option.value}>
+                {option.label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
+
+export default BubbleSelects;
--- components/UserCompnents/Modals/CreateCommunity/Personalizacion.tsx
+++ components/UserCompnents/Modals/CreateCommunity/Personalizacion.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import CurrentStep from "./CurrentStep";
+import { ImageUp } from "lucide-react";
+import BubbleSelects from "@/components/CommonComponents/BubbleSelects";
+import FileUpload from "@/components/kokonutui/file-upload"; // Importa el componente
 
 function Personalizacion() {
-  return <div>Personalizacion</div>;
+  return (
+    <div>
+      <CurrentStep
+        icon={<ImageUp size={48} className="stroke-1 theme-text-purple  " />}
+        title="Personalización"
+        description="Personaliza tu comunidad con una imagen y categoría."
+      />
+
+      <div className="mt-6">
+        <div>
+          {/* Componente de subida de archivos */}
+          <FileUpload />
+        </div>
+
+        <div>
+          <BubbleSelects
+            options={[
+              { label: "Anime", value: "anime" },
+              { label: "Programación", value: "programacion" },
+              { label: "Series", value: "series" },
+              { label: "Videojuegos", value: "videojuegos" },
+              { label: "Ciencia de Datos / IA", value: "data-science" },
+            ]}
+            label="Categoría"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Personalizacion;

fab52692af63b18a44318d21a4fa6513addcb51f
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2025-09-15 00:51:26 -0400

    prepping some stuff for tomorrow 🙂

--- store/useAppStore.ts
+++ store/useAppStore.ts
@@ -1,3 +1,6 @@
 import { create } from "zustand";
+import { createAuthSlice, type AuthStateType } from "./useAuthStore";
 
-export const useAppStore = create((...a) => ({}));
+export const useAppStore = create<AuthStateType>((...a) => ({
+  ...createAuthSlice(...a),
+}));
---
+++ store/useAuthStore.ts
@@ -0,0 +1,5 @@
+import { StateCreator } from "zustand";
+
+export type AuthStateType = {};
+
+export const createAuthSlice: StateCreator<AuthStateType> = (set, get) => ({});

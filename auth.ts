a6d5d395760bb5276de82e87257af340255efdd6
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2025-09-17 09:46:28 -0400

    Trying to figure out the 'right' way to centralize NextAuth config... again 🙃

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
---
+++ components/AuthComponents/AuthForm.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+function AuthForm() {
+  return <div>AuthForm</div>;
+}
+
+export default AuthForm;

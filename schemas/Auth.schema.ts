784e569849a8ff951fe157740e379676dddc9699
Anakinxv <144478975+Anakinxv@users.noreply.github.com>
2025-09-21 17:03:20 -0400

    Wow, another breaking change in the schema. Who could have seen this coming? :)

--- schemas/Auth.schema.ts
+++ schemas/Auth.schema.ts
@@ -17,7 +17,7 @@ export const registerPasswordSchema = z
   .object({
     password: z
       .string()
-      .min(8, "La contraseña debe tener al menos 8 caracteres")
+      .min(8, "La csx ontraseña debe tener al menos 8 caracteres")
       .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
         "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial"
